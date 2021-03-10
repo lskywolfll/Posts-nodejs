9c83be89539c01133f96d7047c9c3dc0b76999c4
Rene <renealejandrosanchezmorales@gmail.com>
2021-03-10 19:32:15 -0300

    se vuelve a utilzar el mysql base

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,5 +1,5 @@
 const ctrl = require('./controller');
-// const store = require('./store');
-const store = require('../../../mysql/remote-mysql');
+const store = require('./store');
+// const store = require('../../../mysql/remote-mysql');
 
 module.exports = ctrl(store);
